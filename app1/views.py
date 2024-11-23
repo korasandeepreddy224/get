e5ad41b6e64c311c64ae46b65ae7c8f7734828c1
korasandeepreddy224 <korasandeepreddy224@gmail.com>
2024-11-23 19:55:33 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def sand(request):
+    return HttpResponse('sand is a substense that can be used lot of works like consructions <h1>25893147</h1>')
